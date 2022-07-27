650ddee48b94a1d87e1604239df4d300db528508
Sean <sean.jennings1@gmail.com>
2022-07-27 05:13:08 -0500

    Improve neogit commit settings

--- nvim/lua/config/neogit.lua
+++ nvim/lua/config/neogit.lua
@@ -1,7 +1,10 @@
 neogit = {}
 
 local function setup ()
-  require('neogit').setup()
+  require('neogit').setup {
+    disable_commit_confirmation = true,
+    disable_insert_on_commit = false,
+  }
 end
 
 neogit.setup = setup
